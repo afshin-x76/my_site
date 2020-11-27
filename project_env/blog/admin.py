8c84f4a17bfcaf0b6872fb877cddcd7ddf92cfdd
afshin <afshin.kh.x76@gmail.com>
2020-11-27 11:39:13 +0000

    Post and Product models created and added to the admin

--- project_env/blog/admin.py
+++ project_env/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PostCategory, Post, ProductCategory, Product
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(Post)
+admin.site.register(PostCategory)
--- project_env/blog/models.py
+++ project_env/blog/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+User = get_user_model()
+
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
+
+
+class PostCategory(models.Model):
+    title = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.title
+
+
+class ArticleBase(models.Model):
+    """
+    this is base of posts and product
+    """
+    thumbnail = models.ImageField()
+    title = models.CharField(max_length=25)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    overview = models.TextField()
+    content = RichTextField()
+    comment = models.IntegerField(default=5)
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(ArticleBase):
+    price = models.IntegerField(default=0)
+    sells = models.IntegerField(default=0)
+    starts = models.IntegerField()
+    active = models.BooleanField(default=True)
+    category = models.ManyToManyField(ProductCategory)
+
+
+class Post(ArticleBase):
+    view = models.IntegerField()
+    like = models.BooleanField(default=False)
+    category = models.ManyToManyField(PostCategory)
